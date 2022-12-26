ead221d811f2089e1e6df28976a46bc60b63abc4
Mark <markrybnikov2000@gmail.com>
2022-12-26 19:23:05 +0300

    Second commit

---
+++ src/main/java/cinema/config/DataInitializer.java
@@ -0,0 +1,32 @@
+package cinema.config;
+
+import cinema.model.Role;
+import cinema.model.User;
+import cinema.service.RoleService;
+import cinema.service.UserService;
+import java.util.Set;
+import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    public DataInitializer(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    public void inject() {
+        Role adminRole = new Role();
+        adminRole.setName(Role.RoleName.ADMIN);
+        roleService.add(adminRole);
+        User user = new User();
+        user.setEmail("admin8@gmail.com");
+        user.setPassword("admin");
+        user.setRoles(Set.of(adminRole));
+        userService.add(user);
+    }
+}
